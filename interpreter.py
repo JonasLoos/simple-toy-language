d3af176302f25a01637160bc8ba9dc5efa688092
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2022-01-30 14:20:45 +0100

    use match case

--- interpreter.py
+++ interpreter.py
@@ -81,50 +81,51 @@ def run_body(body : Tree, names : dict[str, Object]) -> Value:
         elif stmt.data == 'multiline_stmt':
             multiline_stmt, = stmt.children
             assert isinstance(multiline_stmt, Tree), multiline_stmt
-            # do
-            if multiline_stmt.data == 'do_stmt':
-                do_body, = multiline_stmt.children
-                assert isinstance(do_body, Tree), do_body
-                # if block level variable scoope is desired, use `{**names}` instead
-                names["_"] = run_body(do_body, names)
-            # if elif... else
-            elif multiline_stmt.data == 'if_stmt':
-                if_condition, if_body, elifs, else_stmt = multiline_stmt.children
-                assert isinstance(if_condition, Tree), if_condition
-                assert if_condition.data == 'line_stmt', if_condition.data
-                assert isinstance(if_body, Tree), if_body
-                assert if_body.data == 'body', if_body.data
-                assert isinstance(elifs, Tree), elifs
-                assert elifs.data == 'elifs', elifs.data
-                assert isinstance(else_stmt, Tree), else_stmt
-                assert else_stmt.data == 'else', else_stmt.data
-                # handle if
-                conditions = [(if_condition, if_body)]
-                # handle elifs
-                for elif_stmt in elifs.children:
-                    assert isinstance(elif_stmt, Tree), elif_stmt
-                    elif_condition, elif_body = elif_stmt.children
-                    assert isinstance(elif_condition, Tree), elif_condition
-                    assert isinstance(elif_body, Tree), elif_body
-                    conditions += (elif_condition, elif_body),
-                # handle else
-                if len(else_stmt.children) > 0:
-                    else_body, = else_stmt.children
-                    assert isinstance(else_body, Tree), else_body
-                    assert else_body.data == 'body'
-                    # use `true` as condition, to make sure else is executed when no other statement is
-                    true_stmt = Tree('line_stmt', [Tree('thing', [Token('NAME', 'true')])])  # type: ignore
-                    conditions += (true_stmt, else_body),
-                # evaluate all conditions until the correct body to execute is found
-                for condition, stmt_body in conditions:
-                    test_result = run_line_stmt(condition, names)
-                    if test_result.value:  # use python truthiness
-                        names['_'] = run_body(stmt_body, names)
-                        break
-            # error
-            else:
-                # if grammar and interpreter are correct, this point should never be reached
-                raise Exception(f'unknown multiline_stmt: {multiline_stmt}')
+            match multiline_stmt.data:
+                # do
+                case 'do_stmt':
+                    do_body, = multiline_stmt.children
+                    assert isinstance(do_body, Tree), do_body
+                    # if block level variable scoope is desired, use `{**names}` instead
+                    names["_"] = run_body(do_body, names)
+                # if elif... else
+                case 'if_stmt':
+                    if_condition, if_body, elifs, else_stmt = multiline_stmt.children
+                    assert isinstance(if_condition, Tree), if_condition
+                    assert if_condition.data == 'line_stmt', if_condition.data
+                    assert isinstance(if_body, Tree), if_body
+                    assert if_body.data == 'body', if_body.data
+                    assert isinstance(elifs, Tree), elifs
+                    assert elifs.data == 'elifs', elifs.data
+                    assert isinstance(else_stmt, Tree), else_stmt
+                    assert else_stmt.data == 'else', else_stmt.data
+                    # handle if
+                    conditions = [(if_condition, if_body)]
+                    # handle elifs
+                    for elif_stmt in elifs.children:
+                        assert isinstance(elif_stmt, Tree), elif_stmt
+                        elif_condition, elif_body = elif_stmt.children
+                        assert isinstance(elif_condition, Tree), elif_condition
+                        assert isinstance(elif_body, Tree), elif_body
+                        conditions += (elif_condition, elif_body),
+                    # handle else
+                    if len(else_stmt.children) > 0:
+                        else_body, = else_stmt.children
+                        assert isinstance(else_body, Tree), else_body
+                        assert else_body.data == 'body'
+                        # use `true` as condition, to make sure else is executed when no other statement is
+                        true_stmt = Tree('line_stmt', [Tree('thing', [Token('NAME', 'true')])])  # type: ignore
+                        conditions += (true_stmt, else_body),
+                    # evaluate all conditions until the correct body to execute is found
+                    for condition, stmt_body in conditions:
+                        test_result = run_line_stmt(condition, names)
+                        if test_result.value:  # use python truthiness
+                            names['_'] = run_body(stmt_body, names)
+                            break
+                # error
+                case _:
+                    # if grammar and interpreter are correct, this point should never be reached
+                    raise Exception(f'unknown multiline_stmt: {multiline_stmt}')
         # error
         else:
             # if grammar and interpreter are correct, this point should never be reached
@@ -140,77 +141,79 @@ def run_line_stmt(line_stmt : Tree, names : dict[str, Object]) -> Value:
     tmp, = line_stmt.children
     assert isinstance(tmp, Tree), tmp
     line_stmt = tmp
-    # function call
-    if line_stmt.data == 'funccall':
-        name, arguments = line_stmt.children
-        assert isinstance(name, Token), name
-        assert isinstance(arguments, Tree), arguments
-        assert arguments.data == 'comma_list', arguments.data
-        argument_values : list[Value] = []
-        for argument in arguments.children:
-            assert isinstance(argument, Tree), argument
-            argument_values += run_line_stmt(argument, names),
-        if name in names:
-            func = names[name]
-            if isinstance(func, Function):
-                return func(*argument_values)
-            else:
-                raise Fail(f'call of {type(func)} object: {name}', line_stmt)
-        else:
-            raise Fail(f'call of undefined function: {name}', line_stmt)
-    # variable assignment
-    elif line_stmt.data == 'assignment':
-        name, value = line_stmt.children
-        assert isinstance(name, Token), name
-        assert name.type == 'NAME', name.type
-        assert isinstance(value, Tree), value
-        if name in std_names:
-            raise Fail(f'Cannot overwrite a predefined function or value: {name}', name)
-        names[name] = result = run_line_stmt(value, names)
-        return result
-    # thing / value
-    elif line_stmt.data == 'thing':
-        thing, = line_stmt.children
-        assert isinstance(thing, Token), thing
-        if thing.type == 'NAME':
-            if thing in names:
-                value = names[thing]
-                if isinstance(value, Value):
-                    return value
+    match line_stmt.data:
+        # function call
+        case 'funccall':
+            name, arguments = line_stmt.children
+            assert isinstance(name, Token), name
+            assert isinstance(arguments, Tree), arguments
+            assert arguments.data == 'comma_list', arguments.data
+            argument_values : list[Value] = []
+            for argument in arguments.children:
+                assert isinstance(argument, Tree), argument
+                argument_values += run_line_stmt(argument, names),
+            if name in names:
+                func = names[name]
+                if isinstance(func, Function):
+                    return func(*argument_values)
                 else:
-                    raise Fail(f'use of {type(Value)} object as Value: {thing}', thing)
+                    raise Fail(f'call of {type(func)} object: {name}', line_stmt)
             else:
-                raise Fail(f'use of undefined name: {thing}', thing)
-        elif thing.type == "STRING":
-            # if it's a format string, format it
-            if thing[0] == '"':
-                try:
-                    return Value(thing[1:-1].format(**get_values(names)))
-                except KeyError as err:
-                    raise Fail(f'Could not find variable {err} used in format string {thing}', thing) from err
-            # otherwise return just the content
-            return Value(thing[1:-1])
-        elif thing.type == "LONG_STRING":
-            # if it's a format string, format it
-            if thing[0] == '"':
-                try:
-                    return Value(thing[3:-3].format(**get_values(names)))
-                except KeyError as err:
-                    raise Fail(f'Could not find variable {err} used in format string {thing}', thing) from err
-            # otherwise return just the content
-            return Value(thing[3:-3])
-        elif thing.type == "DEC_NUMBER":
-            return Value(int(thing))
-        elif thing.type in ["HEX_NUMBER", "BIN_NUMBER", "OCT_NUMBER", "FLOAT_NUMBER", "IMAG_NUMBER"]:
-            raise Fail(f'{thing.type} is not implemented yet', thing)
+                raise Fail(f'call of undefined function: {name}', line_stmt)
+        # variable assignment
+        case 'assignment':
+            name, value = line_stmt.children
+            assert isinstance(name, Token), name
+            assert name.type == 'NAME', name.type
+            assert isinstance(value, Tree), value
+            if name in std_names:
+                raise Fail(f'Cannot overwrite a predefined function or value: {name}', name)
+            names[name] = result = run_line_stmt(value, names)
+            return result
+        # thing / value
+        case 'thing':
+            thing, = line_stmt.children
+            assert isinstance(thing, Token), thing
+            match thing.type:
+                case 'NAME':
+                    if thing in names:
+                        value = names[thing]
+                        if isinstance(value, Value):
+                            return value
+                        else:
+                            raise Fail(f'use of {type(Value)} object as Value: {thing}', thing)
+                    else:
+                        raise Fail(f'use of undefined name: {thing}', thing)
+                case "STRING":
+                    # if it's a format string, format it
+                    if thing[0] == '"':
+                        try:
+                            return Value(thing[1:-1].format(**get_values(names)))
+                        except KeyError as err:
+                            raise Fail(f'Could not find variable {err} used in format string {thing}', thing) from err
+                    # otherwise return just the content
+                    return Value(thing[1:-1])
+                case "LONG_STRING":
+                    # if it's a format string, format it
+                    if thing[0] == '"':
+                        try:
+                            return Value(thing[3:-3].format(**get_values(names)))
+                        except KeyError as err:
+                            raise Fail(f'Could not find variable {err} used in format string {thing}', thing) from err
+                    # otherwise return just the content
+                    return Value(thing[3:-3])
+                case "DEC_NUMBER":
+                    return Value(int(thing))
+                case "HEX_NUMBER" | "BIN_NUMBER" | "OCT_NUMBER" | "FLOAT_NUMBER" | "IMAG_NUMBER":
+                    raise Fail(f'{thing.type} is not implemented yet', thing)
+                # error
+                case _:
+                    # if grammar and interpreter are correct, this point should never be reached
+                    raise Exception(f'unknown thing type: {thing.type}')
         # error
-        else:
+        case _:
             # if grammar and interpreter are correct, this point should never be reached
-            raise Exception(f'unknown thing type: {thing.type}')
-    # error
-    else:
-        # if grammar and interpreter are correct, this point should never be reached
-        raise Exception(f'unknown line_stmt: {line_stmt}')
+            raise Exception(f'unknown line_stmt: {line_stmt}')
 
 
 
